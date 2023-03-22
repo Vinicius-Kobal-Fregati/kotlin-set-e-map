d0ab19dd8c01c3da40feae8e3c17bb6905411709
Vinícius Kobal Fregati <vfregati@gmail.com>
2023-03-22 16:20:02 -0300

    Testando leitura e filtros no map

--- kotlin-set-e-map/src/main/kotlin/br/com/alura/Main.kt
+++ kotlin-set-e-map/src/main/kotlin/br/com/alura/Main.kt
@@ -1,40 +1,70 @@
 package br.com.alura
 
 fun main() {
-    // O to é um infix do Pair, o mais comum é o to.
-    // O to tem alguns problemas de performance, para algo grande, é recomendado usar o Pair.
-    val pedidos: Map<Int, Double> = mapOf(Pair(1, 20.0), Pair(2, 34.0), 3 to 50.0)
-    println(pedidos)
-    // Pegamos os valores pela chave
-    val pedido: Double? = pedidos[1]
-    pedido?.let { println("Pedido $it") }
+    val pedidos: Map<Int, Double?> = mapOf(
+        Pair(1, 20.0),
+        Pair(2, 34.0),
+        3 to 50.0,
+        4 to null,
+        5 to 150.0,
+        6 to 160.0
+    )
 
-    // No for, temos o tipo como Map.Entry, ele possui a chave e valor do elemento.
-    for (p: Map.Entry<Int, Double> in pedidos) {
-        println("Número do pedido: ${p.key}, " +
-                "valor do pedido: ${p.value}")
+    // Com os três primeiros não dá para saber se o null é por não existir ou se é o valor de fato
+    val valorPedido: Double? = pedidos[4]
+    val valorPedidoComGet: Double? = pedidos.get(4)
+    val chaveInexistenteGet: Double? = pedidos.get(5)
+    val valorPedidoComGetValue: Double? = pedidos.getValue(4)
+    try {
+        // Se a chave não existir, lança exceção
+        val chaveInexistenteGetValue: Double? = pedidos.getValue(5)
+    } catch (e: NoSuchElementException) {
+        println("Chave não existente")
+    }
+    println(valorPedido)
+    println(valorPedidoComGet)
+    println(valorPedidoComGetValue)
+    println(chaveInexistenteGet)
+
+    // Caso não exista, podemos atribuir algo
+    val valorChaveZero: Double? = pedidos.getOrElse(0) {
+        0.0
+    }
+    /*
+    val valorChaveZero: Double? = pedidos.getOrElse(0, {
+        null
+    })
+     */
+    println(valorChaveZero)
+
+    // Se não existir, assume o valor padrão
+    println(pedidos.getOrDefault(1, -1.0))
+    println(pedidos.getOrDefault(0, -1.0))
+
+    // Podemos ter um Collection com os pedidos ou valores
+    println(pedidos.keys)
+    println(pedidos.values)
+
+    for (numeroDePedidos in pedidos.keys) {
+        println("Pedido $numeroDePedidos")
     }
 
-    val pedidosMutaveis: MutableMap<Int, Double> = pedidos.toMutableMap()
-    // A forma mais comum de se inserir é essa primeira, o put não é tanto assim.
-    pedidosMutaveis[4] = 70.0
-    println(pedidosMutaveis)
-    pedidosMutaveis.put(5, 80.0)
-    println(pedidosMutaveis)
-    // Dessa forma também podemos atualizar os valores das chaves
-    pedidosMutaveis[1] = 100.0
-    println(pedidosMutaveis)
+    // Principais formas de fazer filtro no map
+    val pedidosFiltrados = pedidos.filter { (numero, valor) ->
+        numero % 2 == 0
+                && valor != null
+                && valor > 50.0
+    }
 
-    // Adiciona apenas se a chave não existir
-    pedidosMutaveis.putIfAbsent(6, 120.0)
-    println(pedidosMutaveis)
-    // Não faz a adição pois a chave 6 já existe
-    pedidosMutaveis.putIfAbsent(6, 200.0)
-    println(pedidosMutaveis)
+    val pedidosAcima = pedidos.filterKeys { numero ->
+        numero % 2 == 0
+    }
 
-    pedidosMutaveis.remove(6)
-    pedidosMutaveis.remove(3)
-    // Remove apenas se a chave e o valor corresponderem ao passado
-    pedidosMutaveis.remove(1, 110.0)
-    println(pedidosMutaveis)
+    val pedidosPares = pedidos.filterValues { valor ->
+        valor != null && valor > 50.0
+    }
+
+    println(pedidosFiltrados)
+    println(pedidosAcima)
+    println(pedidosPares)
 }
