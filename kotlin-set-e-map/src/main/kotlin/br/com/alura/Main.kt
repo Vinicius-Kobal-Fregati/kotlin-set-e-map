612a9b05ed3a2868dcea8bf4d2930db3f5df141a
Vinícius Kobal Fregati <vfregati@gmail.com>
2023-03-22 15:25:35 -0300

    Testando comportamentos de escrita do map

--- kotlin-set-e-map/src/main/kotlin/br/com/alura/Main.kt
+++ kotlin-set-e-map/src/main/kotlin/br/com/alura/Main.kt
@@ -10,8 +10,31 @@ fun main() {
     pedido?.let { println("Pedido $it") }
 
     // No for, temos o tipo como Map.Entry, ele possui a chave e valor do elemento.
-    for (pedido: Map.Entry<Int, Double> in pedidos) {
-        println("Número do pedido: ${pedido.key}, " +
-                "valor do pedido: ${pedido.value}")
+    for (p: Map.Entry<Int, Double> in pedidos) {
+        println("Número do pedido: ${p.key}, " +
+                "valor do pedido: ${p.value}")
     }
+
+    val pedidosMutaveis: MutableMap<Int, Double> = pedidos.toMutableMap()
+    // A forma mais comum de se inserir é essa primeira, o put não é tanto assim.
+    pedidosMutaveis[4] = 70.0
+    println(pedidosMutaveis)
+    pedidosMutaveis.put(5, 80.0)
+    println(pedidosMutaveis)
+    // Dessa forma também podemos atualizar os valores das chaves
+    pedidosMutaveis[1] = 100.0
+    println(pedidosMutaveis)
+
+    // Adiciona apenas se a chave não existir
+    pedidosMutaveis.putIfAbsent(6, 120.0)
+    println(pedidosMutaveis)
+    // Não faz a adição pois a chave 6 já existe
+    pedidosMutaveis.putIfAbsent(6, 200.0)
+    println(pedidosMutaveis)
+
+    pedidosMutaveis.remove(6)
+    pedidosMutaveis.remove(3)
+    // Remove apenas se a chave e o valor corresponderem ao passado
+    pedidosMutaveis.remove(1, 110.0)
+    println(pedidosMutaveis)
 }
