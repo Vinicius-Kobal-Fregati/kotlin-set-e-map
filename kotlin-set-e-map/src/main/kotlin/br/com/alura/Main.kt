ff36e161694215fd360fcbc2e8ca68ee8f015761
Vinícius Kobal Fregati <vfregati@gmail.com>
2023-03-22 10:40:19 -0300

    Introducao a collections

--- kotlin-set-e-map/src/main/kotlin/br/com/alura/Main.kt
+++ kotlin-set-e-map/src/main/kotlin/br/com/alura/Main.kt
@@ -1,5 +1,28 @@
 package br.com.alura
 
 fun main() {
+    val nomes: Iterable<String> = setOf(
+        "Alex",
+        "Fran",
+        "Gui",
+        "Maria",
+        "Ana"
+    )
 
+    for (nome in nomes) {
+        println(nome)
+    }
+
+    println(nomes)
+    println("Tem o nome Vinícius? ${nomes.contains("Vinícius")}")
+
+    val nomesCollection: Collection<String> = setOf(
+        "Alex",
+        "Fran",
+        "Gui",
+        "Maria",
+        "Ana"
+    )
+
+    println("Tamanho da coleção ${nomesCollection.size}")
 }
